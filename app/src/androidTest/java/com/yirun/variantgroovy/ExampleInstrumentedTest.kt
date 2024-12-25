237b6be5f7b9422586b78c407d48a7b1716e6b4b
yirun <15109175+yirunkong@user.noreply.gitee.com>
2024-12-26 05:50:24 +0800

    first commit

---
+++ app/src/androidTest/java/com/yirun/variantgroovy/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.yirun.variantgroovy
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.yirun.variantgroovy", appContext.packageName)
+    }
+}
